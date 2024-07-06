c3b5e97a7e5f2f206537132c42c7cd9abc08c679
The Raccoon <benkim14030@gmail.com>
2024-07-05 23:01:46 -0700

    first iteration of the port scanner complete

--- PS.py
+++ PS.py
@@ -1,9 +1,40 @@
+
 import socket
-import subprocess
 import sys
 from datetime import datetime
 
 
-#Ask for input
-remoteServer = input("Enter a remote host to scan: ")
-remoteServerIP = socket.gethostbyname(remoteServer)
+target = input(str("Target IP: "))
+
+
+def lines():
+    print("_"*50)
+
+
+# Banner
+lines()
+print("Scanner Target: "+ target)
+print("Scanning started at: " + str(datetime.now()))
+lines()
+
+
+
+try:
+    # Scan every port on the target IP
+    for port in range(1,65535):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        
+        # Return open port
+        result = s.connect_ex((target,port))
+        if result == 0:
+            print("[*] Port {} is open".format(port))
+            
+        s.close()
+        
+except KeyboardInterrupt:
+    print("\n Exiting...")
+    
+except socket.error:
+    print("\n Host not responding...")
+    sys.exit()
